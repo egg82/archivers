27c6a069392ab6d91e8e95e0547f03f30ab04c5c
egg82 <contact.egg82@pm.me>
2025-07-15 20:37:09 -0600

    Add SBOMs via build script

--- gov_archiver/build.sh
+++ gov_archiver/build.sh
@@ -4,12 +4,18 @@ set -euo pipefail
 
 VERSION=1.3.0
 DOCKERFILES=(ubi9-micro ubi9-minimal scratch alpine)
+SBOM_DIR=sboms
 
-docker buildx create --use --name sbom-builder || true
+docker buildx create --use --name sbom-builder >/dev/null 2>&1 || true
+
+mkdir -p "$SBOM_DIR"
 
 for flavor in "${DOCKERFILES[@]}"; do
     IMAGE=egg82/gov_archiver:${flavor}-$VERSION
     DOCKERFILE=Dockerfile-$flavor
+    OUT_SBOM="${SBOM_DIR}/${flavor}-${VERSION}.sbom.json"
 
     docker buildx build --builder sbom-builder --tag "$IMAGE" --sbom true --push -f "$DOCKERFILE" .
+
+    docker buildx imagetools inspect "$IMAGE" --format '{{ json .SBOM }}' > "$OUT_SBOM"
 done
